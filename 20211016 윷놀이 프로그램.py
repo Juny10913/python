1fccb0b74b40ab2add7fa3ae8f1d9687d0e0d088
Juny10913 <97964799+Juny10913@users.noreply.github.com>
2022-02-25 13:39:58 +0900

    Add files via upload

---
+++ 20211016 윷놀이 프로그램.py
@@ -0,0 +1,22 @@
+#윷놀이 프로그램 
+
+a,b,c,d = input("입력:").split(",")
+e = int(a)+int(b)+int(c)+int(d)
+
+if e==0:
+    print("윷")
+elif e==1:
+    print("도")
+elif e==2:
+    print("개")
+elif e==3:
+    print("걸")
+elif e==4:
+    print("모")
+else:
+    print("다시 입력하시오") 
+    
+    
+        
+            
+    
